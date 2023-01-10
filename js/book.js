79b0817fb90698e50317b550de6017a768db0a52
peimanhp <peiman_hp@yahoo.com>
2023-01-11 02:40:57 +0330

    create modal with js

--- js/book.js
+++ js/book.js
@@ -35,38 +35,9 @@ Book.prototype.info = function () {
 
 let myLibrary = [];
 
-newItem.addEventListener("click", showModal);
-function showModal() {
-  overlay.classList.add("show");
-  modal.classList.add("show");
-}
-
-overlay.addEventListener("click", removeModal);
-function removeModal(e) {
-  if (overlay !== e.target) return;
-  overlay.classList.remove("show");
-  modal.classList.remove("show");
-}
-
-submit.addEventListener("click", addNewBookToArray);
+newItem.addEventListener("click", createModal);
 
 let bookNumber = 1;
-
-function addNewBookToArray(e) {
-  const newBook = new Book(
-    bookNumber,
-    newTitle.value,
-    newAuthor.value,
-    newPages.value,
-    isRead.checked,
-    "book" + bookNumber
-  );
-  myLibrary.push(newBook);
-  console.log(myLibrary);
-
-  addBookToLibrary();
-  e.preventDefault();
-}
 
 let books = [];
 
@@ -88,10 +59,6 @@ function addBookToLibrary() {
   const bookElement = document.getElementById(`book${bookNumber - 1}`);
   books.push(bookElement);
   console.log(books);
-
-  modal.reset();
-  overlay.classList.remove("show");
-  modal.classList.remove("show");
 }
 
 function removeBook(e) {
@@ -121,4 +88,116 @@ function changeReadStatus(e) {
   let indexInArray = myLibrary.findIndex((x) => x.bookId === bookId);
   if (myLibrary[indexInArray].read) myLibrary[indexInArray].read = false;
   else myLibrary[indexInArray].read = true;
+}
+
+// modal --> new item window
+function createModal() {
+  const hideModalOnSecape = function (e) {
+    if (e.key == "Escape") {
+      hideModal();
+    }
+  };
+
+  function hideModal() {
+    modalContainer.classList.remove("show");
+    form.classList.remove("show");
+    setTimeout(() => {
+      modalContainer.remove();
+    }, 500);
+    document.removeEventListener("keyup", hideModalOnSecape);
+  }
+
+  setTimeout(() => {
+    modalContainer.classList.add("show");
+    form.classList.add("show");
+  }, 1);
+
+  const modalContainer = document.createElement("div");
+  modalContainer.classList.add(
+    "overlay",
+    "rounded-3",
+    "d-flex",
+    "justify-content-center",
+    "align-items-center"
+  );
+
+  modalContainer.setAttribute("id", "overlay");
+  document.body.appendChild(modalContainer);
+
+  const form = document.createElement("form");
+  form.classList.add("my-modal", "shadow", "text-center");
+  modalContainer.appendChild(form);
+
+  const modalTitle = document.createElement("h3");
+  modalTitle.classList.add("mb-4");
+  modalTitle.innerText = "Add new book";
+  modalTitle.setAttribute("id", "add_new_book");
+  form.appendChild(modalTitle);
+
+  const titleInput = document.createElement("input");
+  titleInput.classList.add("form-control", "my-3");
+  titleInput.setAttribute("placeholder", "Title");
+  titleInput.setAttribute("id", "new_title");
+  form.appendChild(titleInput);
+
+  const authorInput = document.createElement("input");
+  authorInput.classList.add("form-control", "my-3");
+  authorInput.setAttribute("placeholder", "Author");
+  authorInput.setAttribute("id", "new_author");
+  form.appendChild(authorInput);
+
+  const pagesInput = document.createElement("input");
+  pagesInput.classList.add("form-control", "my-3");
+  pagesInput.setAttribute("placeholder", "Pages");
+  pagesInput.setAttribute("id", "new_pages");
+  form.appendChild(pagesInput);
+
+  const readContainer = document.createElement("div");
+  readContainer.classList.add("d-flex", "justify-content-center");
+  form.appendChild(readContainer);
+
+  const readTitle = document.createElement("p");
+  readTitle.innerText = "have you read it?";
+  readContainer.appendChild(readTitle);
+
+  const modalReadCheck = document.createElement("input");
+  modalReadCheck.classList.add("form-check-input", "ms-3");
+  modalReadCheck.setAttribute("type", "checkbox");
+  modalReadCheck.setAttribute("value", "false");
+  modalReadCheck.setAttribute("id", "read_it");
+  readContainer.appendChild(modalReadCheck);
+
+  const submitBtn = document.createElement("input");
+  submitBtn.classList.add("btn", "btn-outline-success", "px-4");
+  submitBtn.setAttribute("type", "submit");
+  submitBtn.setAttribute("value", "submit");
+  submitBtn.setAttribute("id", "submit");
+  form.appendChild(submitBtn);
+
+  modalContainer.addEventListener("click", removeModal);
+  function removeModal(e) {
+    if (modalContainer !== e.target) return;
+    hideModal();
+  }
+
+  document.addEventListener("keyup", hideModalOnSecape);
+
+  submitBtn.addEventListener("click", addNewBookToArray);
+
+  function addNewBookToArray(e) {
+    const newBook = new Book(
+      bookNumber,
+      titleInput.value,
+      authorInput.value,
+      pagesInput.value,
+      modalReadCheck.checked,
+      "book" + bookNumber
+    );
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+
+    addBookToLibrary();
+    hideModal();
+    e.preventDefault();
+  }
 }
